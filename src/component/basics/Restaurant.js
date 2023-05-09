0a558bfd42fc8a7207a368a6aa04f58e9588be13
prathamgx <prathamgoswamipg@gmail.com>
2023-05-09 15:14:23 +0530

    project done

---
+++ src/component/basics/Restaurant.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {useState} from "react"
+import "./style.css"
+import MenuCard from './MenuCard'
+import Menu from './menuApi'
+import NavBar from './NavBar'
+
+const uniqueList=[...new Set(Menu.map((curElem)=>{
+    return curElem.category
+})),"All"]
+
+
+const Restaurant = () => {
+    const [menuData,setMenuData]=useState(Menu) //hook used
+    const [menuList,setMenuList]=useState(uniqueList)
+
+    const filterItem = (category) => {
+        if (category==="All"){
+            setMenuData(Menu)
+            return;
+        }
+        const updatedList = Menu.filter((curElem)=>{
+            return curElem.category === category;
+        })
+    
+        setMenuData(updatedList);
+    }
+
+  return (
+    <>
+        <NavBar filterItem={filterItem} menuList={menuList}/>
+        <MenuCard menuData={menuData} /> 
+    </>
+  )
+}
+export default Restaurant
